ba65ba06fe63200241a4d613eea805daf6adb9c0
42azimut <azimutuniverse@gmail.com>
2021-06-24 21:53:39 +0900

    coding apple useState

--- algorithm/react_JS_elice/elice_study/src/App.js
+++ algorithm/react_JS_elice/elice_study/src/App.js
@@ -1,25 +1,26 @@
+import React, { useState } from 'react';
 import './App.css';
 
 
 function App() {
 
-  let posts = '스테이크 맛집'
-  let post_style = { color: 'blue', fontSize: '30px' }
+  let [글제목, 글제목변경] = useState(['남자aaa 코트 추천', ['송파 aaaa스테이크']]); // [state 데이터, state 데이터 변경 함수]
+  let [like, likeUpdate] = useState(0);
+  let posts = '강남 고기 맛집';
 
-  function fn100() {
-    return 100
-  }
+  
   return (
     <div className="App">
       <div className="black-nav">
-        <div className={ posts }>blog study for React</div>
-        <div style={ post_style }>style dev</div>
+        <div>개발 블로그</div>
       </div>
-        <h4> { posts } </h4>
-        <h4> { fn100() } </h4>
+      <div className="list">
+        <h3> { 글제목[0] } <span onClick={ () => { likeUpdate( like + 1) } }>❤️</span> { like } </h3>
+        <p>6월 25일 발행</p>
+        <hr/>
+      </div>
     </div>
   );
 }
 
 export default App;
-                           
