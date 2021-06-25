debb2dce43d1b031e58f26896a2dfb6ff26aec1b
42azimut <azimutuniverse@gmail.com>
2021-06-25 23:14:02 +0900

    component and modal

--- algorithm/react_JS_elice/elice_study/src/App.js
+++ algorithm/react_JS_elice/elice_study/src/App.js
@@ -10,25 +10,51 @@ function App() {
   let [like, likeUpdate] = useState(0);
   let posts = '강남 고기 맛집';
 
-  function titleUpdate() {
-    let newArr = [...글제목];   //deep copy
-    newArr[0] = '여자코트 추천'
-    글제목변경(newArr)
-  }
+  // function titleUpdate() {
+  //   let newArr = [...글제목];   //deep copy
+  //   newArr[0] = '여자코트 추천'
+  //   글제목변경(newArr)
+  // }
   
+
+
   return (
     <div className="App">
       <div className="black-nav">
         <div>개발 블로그</div>
       </div>
-      <button onClick={ titleUpdate }> title update 버튼</button>
+
+      <Modal></Modal>
+
       <div className="list">
         <h3> { 글제목[0] } <span onClick={ () => { likeUpdate( like + 1) } }>❤️</span> { like } </h3>
         <p>6월 25일 발행</p>
         <hr/>
       </div>
+
+      <div className="list">
+        <h3>{ 글제목[1] }</h3>
+        <p>3월 22일</p>
+        <hr />
+      </div>
+
+      <div className="list">
+        <h3>{ 글제목[2] }</h3>
+        <p>12월 26일</p>
+        <hr />
+      </div>
     </div>
   );
+}
+
+function Modal() {   
+  return (
+    <div className="modal">
+      <h2>제목 modal</h2>
+      <p>날짜</p>
+      <p>상세 내용</p>
+  </div>
+  )
 }
 
 export default App;
