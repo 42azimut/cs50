0cf929e491e2ca238b7da3213b32bf8bc640481b
42azimut <azimutuniverse@gmail.com>
2021-06-26 22:09:05 +0900

    누적 확진자 수 confirmedData

--- react_covid/src/components/Contents.js
+++ react_covid/src/components/Contents.js
@@ -7,26 +7,66 @@ const Contents = () => {
   const [confirmedData, setConfirmedData] = useState({})
 
   useEffect(() => {
-    
     const fetchEvents = async () => {
       const res = await axios.get("https://api.covid19api.com/total/dayone/country/kr")
-      console.log(res);
+      makeData(res.data)
+    }
+    const makeData = (items) => {
+      const arr = items.reduce((acc, cur) => {
+        const currentDate = new Date(cur.Date);
+        const year = currentDate.getFullYear();
+        const month = currentDate.getMonth();
+        const date = currentDate.getDate();
+        const confirmed = cur.Confirmed;
+        const active = cur.Active;
+        const death = cur.Death;
+        const recoverd = cur.Recovered;
+
+        const findItem = acc.find(a => a.year === year && a.month === month);
+        if(!findItem) {
+          acc.push({year, month, date, confirmed, active, death, recoverd})
+        }
+        if (findItem && findItem.date < date) {
+          findItem.active = active;
+          findItem.death = death;
+          findItem.date = date;
+          findItem.year = year;
+          findItem.month = month;
+          findItem.recoverd = recoverd;
+          findItem.confirmed = confirmed;
+        }
+        return acc;
+      }, [])
+
+      const lebels = arr.map(a => `${a.month+1}월`)
+      setConfirmedData({
+        labels: lebels,
+        datasets: [
+          {
+            label: "국내 누적 확진자", 
+            backgroundColor: "salmon",
+            fill:true,
+            data: arr.map(a => a.confirmed)
+          },
+        ]
+      })
+      
     }
     fetchEvents()
   })
 
   return (
-  <section>
-    <h2>국내 코로나 현황</h2>
-    <div className="contents">
-       <div>
-         <Bar data={confirmedData} options={
-           { title: { display: true, text: "누적 확진자 추이", fontSize: 16 }},
-           { legend: {display: true, position: "bottom" }}
-         } />
-       </div>
-    </div>
-  </section>
+    <section>
+      <h2>국내 코로나 현황</h2>
+      <div className="contents">
+        <div>
+          <Bar data={confirmedData} options={
+            { title: { display: true, text: "누적 확진자 추이", fontSize: 16 }},
+            { legend: { display: true, position: "bottom" }}
+          } />
+        </div>
+      </div>
+    </section>
   )
 }
 
