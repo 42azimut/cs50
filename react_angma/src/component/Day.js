61e99cd8bef72d00c339c5f05ed7a9b167b51196
42azimut <azimutuniverse@gmail.com>
2021-07-04 22:28:17 +0900

    BrowserRouter, Route, Switch etc...

--- react_angma/src/component/Day.js
+++ react_angma/src/component/Day.js
@@ -1,12 +1,13 @@
 import dummy from "../db/data.json" 
+import { useParams } from "react-router";
 
 export default function Day() {
   // dummy.words
-  const day = 3;
-  const wordList = dummy.words.filter(word => word.day === day);
-
+  const { day } = useParams();
+  const wordList = dummy.words.filter(word => word.day === Number(day));
   return (
     <>
+    <h2>Day {day}</h2>
       <table>
         <tbody>
           {wordList.map(word => (
