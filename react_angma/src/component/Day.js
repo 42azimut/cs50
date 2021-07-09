3061386ec53ea1e71699e66bb313a931de415a74
42azimut <azimutuniverse@gmail.com>
2021-07-09 22:00:06 +0900

    error - 6:21 DayList.js useEffect error

--- react_angma/src/component/Day.js
+++ react_angma/src/component/Day.js
@@ -1,17 +1,29 @@
-import dummy from "../db/data.json" 
 import { useParams } from "react-router";
 import Word from "./Word"
+import { useEffect, useState } from "react"
 
 export default function Day() {
   // dummy.words
   const { day } = useParams();
-  const wordList = dummy.words.filter(word => word.day === Number(day));
+  // const wordList = dummy.words.filter(word => word.day === Number(day));
+  const [words, setWords] = useState([])
+
+  useEffect(() => {
+    fetch("http://localhost:3000/words")
+      .then(res => {
+        return res.json()
+      })
+      .then(data => {
+        setWords(data)
+      });
+  }, []); 
+  
   return (
     <>
     <h2>Day {day}</h2>
       <table>
         <tbody>
-          {wordList.map(word => (
+          {words.map(word => (
           <Word word={word} key={word.id} />
           ))}
         </tbody>
--- react_angma/src/component/DayList.js
+++ react_angma/src/component/DayList.js
@@ -1,32 +1,20 @@
-import dummy from "../db/data.json"
-import { Link } from "react-router-dom"
 import { useEffect, useState } from "react"
+import { Link } from "react-router-dom"
 
 export default function DayList() {
   const [days, setDays] = useState([])
-  const [count, setCount] = useState(0)
-
-  function onClick() {
-    setCount(count + 1)
-  }
-  
-  function onClick2() {
-    setDays([
-      ...days,
-      {
-        id: Math.random(),
-        day : 1,
-      }
-    ])
-  }
-
 
   useEffect(() => {
-    console.log("count change!!!");
-  }, [])   //count 가 변경되었을때만 실행 : 의존성 배열!!
+    fetch("http://localhost:3000/days")
+      .then(res => {
+        return res.json()
+      })
+      .then(data => {
+        setDays(data)
+      });
+  }, []);   //count 가 변경되었을때만 실행 : 의존성 배열!!
 
   return (
-    <>
       <ul className="list_day">
         {days.map(day => (
           <li key={day.id}>
@@ -34,8 +22,5 @@ export default function DayList() {
           </li>
         ))}
       </ul> 
-      <button onClick={onClick}>{ count }</button>
-      <button onClick={onClick2}>Day change</button>
-    </>
   );
 }
